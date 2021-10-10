964c70cf80a7e644247fc26b00ad8fbf19fdab39
TJIquin <201980256@psu.palawan.edu.ph>
2021-10-11 02:00:45 +0800

    inc directory done

---
+++ cover-verbose-memory/features.php
@@ -0,0 +1,10 @@
+<?php include 'inc/header.php'; ?>
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">FEATURES.</h1>
+        <p class="lead">Intuitive, Optimized,Easy to Use, Flexible, Integrated, Geared, Media Support, High Performance, Scalable, Quality Content, Bonus Features</p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+
+    <?php include 'inc/footer.php'; ?>
